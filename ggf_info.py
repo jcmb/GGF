abadcae0ed379053eb833dab8a19a6fb558fb68a
Geoffrey Kirk <jcmb@users.noreply.github.com>
2022-09-20 15:55:43 -0600

    Strict and Plotting
    Add option to strictly parse the file and complain if wrong, otherwise use defaults.
    
    Also added support for plotting the GGF file and saving as a a image.

--- ggf_info.py
+++ ggf_info.py
@@ -7,7 +7,7 @@ import argparse
 from struct import *
 import json
 
-from matplotlib import pyplot as plt
+
 
 class GGF:
 
@@ -21,6 +21,10 @@ class GGF:
     @property
     def errorString(self):
          return self._errorString
+
+    @property
+    def Grid(self):
+         return self._grid
 
     @property
     def LatInterval(self):
@@ -80,8 +84,16 @@ class GGF:
          return self._MinValue
 
     @property
+    def MinValueFooter(self):
+         return self._MinValueFooter
+
+    @property
     def MaxValue(self):
          return self._MaxValue
+
+    @property
+    def MaxValueFooter(self):
+         return self._MaxValueFooter
 
     @property
     def Name(self):
@@ -106,7 +118,7 @@ class GGF:
 #        print((b & testBit) != 0)
         return ((b & testBit) != 0)
 
-    def parseFlags(self,flags):
+    def parseFlags(self,flags,strict):
         self._flags={}
 
 #        pprint(flags)
@@ -121,8 +133,6 @@ class GGF:
 
 #       Flags 1 is documented in the standard but it is ignored in all the Trimble readers
 
-        if flags[1]==0:
-            return(False,101,"Units not set")
         self._flags["GIF_UNITS_MILLIMETERS"]=self.bitSet(flags[1],0)
         self._flags["GIF_UNITS_CENTIMETERS"]=self.bitSet(flags[1],1)
         self._flags["GIF_UNITS_METERS"]=self.bitSet(flags[1],2)
@@ -130,6 +140,12 @@ class GGF:
         self._flags["GIF_UNITS_SURVEY_FEET"]=self.bitSet(flags[1],4)
         self._flags["GIF_UNITS_INTL_INCHES"]=self.bitSet(flags[1],5)
         self._flags["GIF_UNITS_INTL_FEET"]=self.bitSet(flags[1],6)
+
+        if flags[1]==0:
+            if strict:
+                return(False,101,"Units not set")
+            else:
+                self._flags["GIF_UNITS_METERS"]=True
 
 
         if flags[2]==0: #Really should have a striuck option
@@ -157,20 +173,26 @@ class GGF:
         self._flags["GIF_FORMAT_LONG_DOUBLE"]=self.bitSet(flags[3],5)
 
 
-#        if flags[4]==0:
-#            return(False,104,"Lat direction not set")
         self._flags["GIF_LAT_ASCENDING"]=self.bitSet(flags[4],0)
         self._flags["GIF_LAT_DESCENDING"]=self.bitSet(flags[4],1)
         self._flags["GIF_LAT_NOT_ASCENDING"]=not self._flags["GIF_LAT_ASCENDING"]
+        if flags[4]==0:
+            if strict:
+                return(False,104,"Lat direction not set")
+            else:
+                self._flags["GIF_LAT_ASCENDING"]=True
 
 #        self._flags["GIF_LAT_NORTHINGS"]=self.bitSet(flags[4],2)
 #        self._flags["GIF_LAT_SOUTHINGS"]=self.bitSet(flags[4],3)
 
-#        if flags[5]==0:
-#            return(False,105,"Long direction not set")
         self._flags["GIF_LON_ASCENDING"]=self.bitSet(flags[5],0)
         self._flags["GIF_LON_DESCENDING"]=self.bitSet(flags[5],1)
         self._flags["GIF_LON_NOT_ASCENDING"]=not self._flags["GIF_LON_ASCENDING"]
+        if flags[5]==0:
+            if strict:
+                return(False,105,"Long direction not set")
+            else:
+                self._flags["GIF_LON_ASCENDING"]=True
 #        self._flags["GIF_LON_EASTINGS"]=self.bitSet(flags[5],2)
 #        self._flags["GIF_LON_WESTINGS"]=self.bitSet(flags[5],3)
 
@@ -179,6 +201,8 @@ class GGF:
 
     def parseGrid(self, ggfFile):
         self._grid=[]
+        self._MinValue=None
+        self._MaxValue=None
         for lat in range(self._LatGridSize):
             start=self.GRID_HEADER_LENGTH + lat * self._LongGridSize * 4
             end=self.GRID_HEADER_LENGTH + (lat+1) * self._LongGridSize * 4
@@ -191,12 +215,17 @@ class GGF:
             for longs_index in range(len(longs)):
                 if longs[longs_index]==self._GridMissing:
                     longs[longs_index]=None
+                else:
+                    if self._MinValue == None or longs[longs_index] < self._MinValue:
+                        self._MinValue = longs[longs_index]
+                    if self._MaxValue == None or longs[longs_index] > self._MaxValue:
+                        self._MaxValue = longs[longs_index]
 #            pprint(longs)
             self._grid.append(longs)
 
 
 
-    def validateAndParse(self, ggfFile):
+    def validateAndParse(self, ggfFile,strict):
         # Validates the basic parts of GGF file.
         if len(ggfFile) < 146:
             return(False,1,"File to small to have the whole header")
@@ -226,12 +255,10 @@ class GGF:
         self._LatGridSize=unpack("<I",ggfFile[96:100])[0]
         self._LongGridSize=unpack("<I",ggfFile[100:104])[0]
 
-# TODO: need to deal with rounding issue of floating point.
-
-        if self._LatMin+(self._LatGridSize-1)*self._LatInterval != self._LatMax:
+        if abs((self._LatMin+(self._LatGridSize-1)*self._LatInterval) - self._LatMax) > 0.0001:
             return(False,4,"Lat Grid is inconsisten with Min and Step")
 
-        if self._LongMin+(self._LongGridSize-1)*self._LongInterval != self._LongMax:
+        if abs(self._LongMin+(self._LongGridSize-1)*self._LongInterval - self._LongMax) > 0.0001:
             return(False,5,"Long Grid is inconsisten with Min and Step")
 
 
@@ -242,7 +269,7 @@ class GGF:
 
         self._GridWindow=unpack("<H",ggfFile[136:138])[0]
 
-        (valid, errNum, errString) = self.parseFlags(ggfFile[138:146])
+        (valid, errNum, errString) = self.parseFlags(ggfFile[138:146],strict)
         if not valid:
             return (valid, errNum, errString)
 
@@ -259,16 +286,16 @@ class GGF:
         elif version == 1:
             if len(ggfFile) != gridSize + self.GRID_HEADER_LENGTH+16:
                 return(False,6,"File size not consistent with the grid. V1")
-            self._MinValue=unpack("<d",ggfFile[gridSize + self.GRID_HEADER_LENGTH:gridSize + self.GRID_HEADER_LENGTH+8])[0]
-            self._MaxValue=unpack("<d",ggfFile[gridSize + self.GRID_HEADER_LENGTH+8:gridSize + self.GRID_HEADER_LENGTH+16])[0]
+            self._MinValueFooter=unpack("<d",ggfFile[gridSize + self.GRID_HEADER_LENGTH:gridSize + self.GRID_HEADER_LENGTH+8])[0]
+            self._MaxValueFooter=unpack("<d",ggfFile[gridSize + self.GRID_HEADER_LENGTH+8:gridSize + self.GRID_HEADER_LENGTH+16])[0]
 
         self.parseGrid(ggfFile)
 
         return(True,0,"")
 
-    def __init__(self, ggfFile):
+    def __init__(self, ggfFile,strict):
         self._version=None
-        (self._valid,self._errorNumber,self._errorString)=self.validateAndParse(ggfFile)
+        (self._valid,self._errorNumber,self._errorString)=self.validateAndParse(ggfFile,strict)
 
     def dump(self,output):
         print(f"Version: {self.version}",file=output)
@@ -283,6 +310,8 @@ class GGF:
         print(f"Missing: {self.GridMissing}",file=output)
         print(f"Scalar:  {self.GridScalar}",file=output)
         print(f"Window:  {self.GridWindow}",file=output)
+        print(f"Min Values:  {self.MinValue:.3f} from Footer {self.MinValueFooter:.3f}",file=output)
+        print(f"Max Values:  {self.MaxValue:.3f} from Footer {self.MaxValueFooter:.3f}",file=output)
         pprint(self._flags,stream=output)
 
     def JSON(self):
@@ -309,27 +338,56 @@ def get_args():
     parser = argparse.ArgumentParser(fromfile_prefix_chars="@",description='Remote.It Account Summary.')
 
     parser.add_argument("GGF", type=argparse.FileType('rb'), help="GGF File to display info on",)
+    parser.add_argument('--strict', action='store_true', help='Check the file for being strictly valid.\nIf not given use default values')
     parser.add_argument('--json', action='store_true', help='Output in JSON format.')
+    parser.add_argument('--plot', action='store_true', help='Create a plot of the data.')
+    parser.add_argument('--image', action='store_true', help='Create a image of the data.')
     parser = parser.parse_args()
     return (vars(parser))
 
 
 def main():
     args=get_args()
-    ggf=GGF(args["GGF"].read())
+
+    ggf=GGF(args["GGF"].read(),args["strict"])
     if ggf.valid:
         if args["json"]:
             json=ggf.JSON()
             print(json)
+        elif args["plot"] or args["image"]:
+            from matplotlib import pyplot as plt
+            import numpy as np
+
+            y = []
+            for i in range (ggf.LatGridSize):
+                y.append(ggf.LatMin + i*ggf.LatInterval)
+#            pprint(x)
+#            y = np.arange(start = ggf.LatMin,stop= ggf.LatMin+(ggf.LatGridSize)*ggf.LatInterval ,step=ggf.LatInterval)
+#            x = np.arange(start = ggf.LongMin,stop= ggf.LongMin+(ggf.LongGridSize)*ggf.LongInterval ,step=ggf.LongInterval)
+            x = []
+            for i in range (ggf.LongGridSize):
+                x.append(ggf.LongMin + i*ggf.LongInterval)
+
+
+            if args["image"]:
+                plt.figure(figsize=(12,9),dpi=200)
+            else:
+                plt.figure(figsize=(12,9))
+            plt.ylabel('Latitude (Degrees)')
+            plt.xlabel('Longitude (Degrees)')
+            plt.title(ggf.Name)
+            h = plt.contourf(x, y, ggf.Grid)
+            plt.colorbar(h,label="Seperation (Meters)")
+            if args["image"]:
+                plt.savefig(ggf.Name+".png")
+            else:
+                plt.show()
         else:
             ggf.dump(sys.stdout)
- #       print(f"Values:  Min: {ggf.MinValue}   Max: {ggf.MaxValue}")
-#        pprint(ggf._grid)
+
 
     else:
-        print("Invalid")
-        print(ggf.errorNumber)
-        print(ggf.errorString)
+        print("GGF File is Invalid. Error {}. {}".format(ggf.errorNumber,ggf.errorString))
 #   pprint(ggf)
 
 
